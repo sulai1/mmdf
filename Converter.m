e057c698b5530390d726b4cf772366a56e70a48b
Sascha <sulai@gmx.at>
2016-05-20 21:24:03 +0200

    removed bug that crashed when converting jxr parallel

--- Converter.m
+++ Converter.m
@@ -12,24 +12,32 @@ classdef Converter
     end
     
     methods (Static)
+        
+        %%convert the given input image to out. filending used to detect
+        %format
         function convert(in,out,varargin)
+            Converter.parconvert(in,out,0,varargin);
+        end
+        
+        %% parallel conversion depends on unique id
+        function parconvert(in,out,id,varargin)
+           	tmpid = sprintf('tmp_%d.bmp',id);
             %% conversion args
             quality = 1;
-            quantization = 255;% TODO use quantization
             bGrayscale = 0;
             
             %%check for additional arguments
-            if(nargin>2)
+            if(nargin>3)
                 quality = varargin{1};
                 if(quality<0||quality>1)
                    error('quality has to be between zero and one.\n value = %f', quality);
                 end
-            elseif(nargin>=3)
+            elseif(nargin>=4)
                 quantization = varargin{2};
                 if(quantization<1||quantization>255)
                    error('quality has to be between 1 and 255.\n value = %u', quantization);
                 end
-            elseif(nargin==5)
+            elseif(nargin==6)
                 bGrayscale = varargin{3};
             end    
             
@@ -39,25 +47,26 @@ classdef Converter
             if(outtype==Converter.JXR)
                 if(in(end-3:end)==Converter.BMP)
                     Converter.convertJXR(in,out,quality,bGrayscale);
-                else
-                    Converter.convertMagick(in,'tmp.bmp',quality,bGrayscale);
-                    Converter.convertJXR('tmp.bmp',out,quality,bGrayscale);
-                    delete('tmp.bmp');
+                else % workaround for ~bmp -> jxr
+                    Converter.convertMagick(in,tmpid,quality,bGrayscale);
+                    Converter.convertJXR(tmpid,out,quality,bGrayscale);
+                    delete(tmpid);
                 end
             elseif(intype==Converter.JXR)
                 %% check output file type
                 if strcmp(outtype,Converter.BMP)
                     Converter.convertJXR(in,out,quality,bGrayscale);
-                else
-                    Converter.convertJXR(in,'tmp.bmp',quality,bGrayscale);
-                    Converter.convertMagick('tmp.bmp',out,quality,bGrayscale);
-                    delete('tmp.bmp');
+                else % workaround for jxr -> ~bmp
+                    Converter.convertJXR(in,tmpid,quality,bGrayscale);
+                    Converter.convertMagick(tmpid,out,quality,bGrayscale);
+                    delete(tmpid);
                 end
             else 
                 Converter.convertMagick(in,out,quality,bGrayscale);
             end
         end
-                    
+        
+        %%conversion using ImageMagick
         function convertMagick(in,out,quality,bGrayscale)
             if(bGrayscale==0)
                 system(sprintf('convert %s -quality %u %s',in, round(quality*100), out));
@@ -65,8 +74,10 @@ classdef Converter
                 system(sprintf('convert %s -quality %u -colorspace Gray %s',in, round(quality*100), out));
             end
         end
+        
+        %%conversion using jxrlib's JXREncApp and JXRDecApp
         function convertJXR(in,out,quality,bGrayscale)
-            if(out(end-3:end)==Converter.JXR)
+            if(out(end-3:end)==Converter.JXR)  %handle gray
                 if(bGrayscale~=0)
                     system(sprintf('JxrEncApp -i %s -q %f1.2 -d 0 -o %s',in, quality, out));
                 else
@@ -77,14 +88,19 @@ classdef Converter
             end
         end
         
+        %%convert the image so that it not exceeds the extent if possible
         function convert2size(in,out,extent)
+            Converter.parConvert2size(in,out,0, extent);
+        end
+        
+        function parConvert2size(in,out,id,extent)
             max = 1;
             min = 0;
             while(abs(max-min)>0.01)%there are only 100 quality levels
                 if(max>1)
                     disp('err')
                 end
-                Converter.convert(in, out, max);
+                Converter.parconvert(in, out,id, max);
                 %% convert and check size
                 d = dir(out);
                 size = d(1).bytes;
@@ -106,33 +122,28 @@ classdef Converter
         %it does not extend the given size. If the destination format is
         %not jpg the file is compressed back to lossless jpg so the
         %retrieval demo can handle it.
-        function avgSize = convertDB(srcDir,dstDir,srcFormat,dstFormat,sratio)
-            sum = 0;
-            count = 0;
+        function  convertDB(srcDir,dstDir,srcFormat,dstFormat,sratio)
             mkdir(dstDir);
             names = dir(fullfile(srcDir, ['*.',srcFormat])) ;
             imagenames = {names.name};
             nrimg = numel(names);
-            for i=1: nrimg
+            parfor i=1: nrimg
                src = fullfile(srcDir,imagenames{i});
                dst = fullfile(dstDir,[imagenames{i}(1:end-3),dstFormat]);
                asjpg = [dst(1:end-3),'jpg'];
                if (~exist(asjpg,'file')) %skip if allready in folder to resume in case of interrupt
-                   count = count+1;
                    extent = names(i).bytes*sratio;
-                   Converter.convert2size(src,dst,extent);
+                   Converter.parConvert2size(src,dst,i,extent);
                    listing = dir(dst);
                    fprintf('converting: %d/%d. %s -> %d bytes\n', ...
                         i, nrimg, dst, listing(1).bytes);
-                   sum = sum + listing(1).bytes;  
                    %if we are not compressing to jpg we have to compress it
-                   %back lossles for compatibility with the retrieval demo
+                   %back lossless for compatibility with the retrieval demo
                    if ~strcmp(dstFormat,'jpg')
-                       Converter.convert(dst,asjpg,1.0);
+                       Converter.parconvert(dst,asjpg,i,1.0);
                        delete(dst);
                    end
                end
-               avgSize = sum / count;
             end
         end
     end
