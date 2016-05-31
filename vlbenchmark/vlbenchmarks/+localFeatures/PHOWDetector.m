46f32f23e29bdcd79970fab5ec931b9cc12f60f5
Sascha <sulai@gmx.at>
2016-05-31 12:28:01 +0200

    created readme.txt

---
+++ vlbenchmark/vlbenchmarks/+localFeatures/PHOWDetector.m
@@ -0,0 +1,35 @@
+classdef PHOWDetector < localFeatures.CVDetector
+    %ORBDETECTOR Summary of this class goes here
+    %   Detailed explanation goes here
+    % Step 1 : Extracts a SIFT descriptor each STEP pixels. 
+    % Size 3 : A spatial bin covers SIZE pixels. 
+    
+    properties
+        Opts = struct(...
+            'Sizes', 8, ...
+            'Step',16, ...
+            'Color', 'rgb' ...
+        );
+    end
+    
+    methods
+        
+        function obj = PHOWDetector(varargin)
+            obj = obj@localFeatures.CVDetector('PHOW',varargin);
+        end
+        % extract the descriptors and convert them
+        function [frames descriptors] = extractDescriptors(obj, imagePath, f)
+            [frames, descriptors] = obj.extract(imagePath);
+        end
+        
+        function [features, descriptors] = extract(obj, imagePath)
+            import cv.*
+            [features, descriptors] = vl_phow(single(imread(imagePath)),... 
+            'Sizes', obj.Opts.Sizes, ...
+            'Step',obj.Opts.Step, ...
+            'Color', obj.Opts.Color ...
+            );
+        end
+    end
+end
+
